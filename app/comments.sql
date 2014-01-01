b59189cf0325b9ee854ba32958a4477fbb6130a7
Priswin <pjose@iforceproservices.com>
2014-01-01 08:57:50 -0500

    Added new 'comments' table in database

---
+++ app/comments.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `challenge_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `comment` longtext NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1-active,0-inactive',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
